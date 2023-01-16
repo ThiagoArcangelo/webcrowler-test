f3d9e260baf48e8a5863cba9624d3cb4d6f6d374
ThiagoArcangelo <devtsarcangelo@gmail.com>
2023-01-16 07:43:04 -0300

    Browser close

--- src/controllers/scrapController.js
+++ src/controllers/scrapController.js
@@ -1,38 +1,39 @@
-const puppetter = require('puppeteer');
+const puppetter = require("puppeteer");
 
-const url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops';
+const url =
+  "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops";
 
 exports.get = async (req, res) => {
-
   async function webscrap() {
-    const browser = await puppetter.launch({headless: true});
+    const browser = await puppetter.launch({ headless: false });
     const page = await browser.newPage();
-  
 
     await page.goto(url);
 
     const data = await page.evaluate(() => {
-        const itens = document.querySelectorAll('.thumbnail');
-        const products = Array.from(itens);
-       
+      const itens = document.querySelectorAll(".thumbnail");
+      const products = Array.from(itens);
 
-        return products.filter(product => {
-          return product.querySelector('.title').innerText.toLowerCase().includes('lenovo');
-        }).map(product => {
-            const title = product.querySelector('.title').innerText;
-            const price = product.querySelector('.price').innerText;
-            const description = product.querySelector('.description').innerText;
-            return { title, price, description };
+      return products
+        .filter((product) => {
+          return product
+            .querySelector(".title")
+            .innerText.toLowerCase()
+            .includes("lenovo");
+        })
+        .map((product) => {
+          const title = product.querySelector(".title").innerText;
+          const price = product.querySelector(".price").innerText;
+          const description = product.querySelector(".description").innerText;
+          return { title, price, description };
         });
-      })    
+    });
 
     console.log(data);
-    res.json(data);
+    res.status(200).json(data);
 
-
-    await page.close();    
+    await browser.close();
   }
 
   webscrap();
-}
-
+};
